f89e76d9d0887fe13cef3e0345742f358ab24d01
katoren812 <44137232+katoren812@users.noreply.github.com>
2018-11-18 16:26:55 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,16 @@
 int main(int argc, char *argv[]) 
 {
     char str[5];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
+    int i=0,a[100],z;
 
+    while(fgets(str, sizeof(str), stdin)){
+        z= atoi(str);
+        if(0<=z&&z<=100){
+            a[i]=z;
+            i++;
+        }
+      else if(-100<=z&&z<0)
+            printf("%d\n",a[-z-1]);
+    }
     return 0;
 }
